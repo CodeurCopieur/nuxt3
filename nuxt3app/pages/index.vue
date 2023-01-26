51e78430ac19bf3e7d7bd5b15e3c6b31f67e07c7
Widdy LOUIS <louisws@outlook.fr>
2023-01-27 00:03:44 +0100

    Head Tags SEO tags
    Rendre notre site Web convivial pour le référencement en utilisant les Head components  intégrés dans Nuxt 3 et useHead composable.

--- nuxt3app/pages/index.vue
+++ nuxt3app/pages/index.vue
@@ -1,5 +1,22 @@
+<script setup>
+  // useHead({
+  //   title: 'Home - Nuxt 3 Demo',
+  //   meta: [
+  //     {name: "description", content: "Ceci est la démo de l'application Nuxt 3"},
+  //     {property: "og:title", content: "C'est un titre og"}],
+  //   link: [{rel: "stylesheet", link: "./app.css"}]
+  // });
+
+  const title = ref('Home - Nuxt 3 Demo');
+</script>
 <template>
   <div>
+    <Head>
+      <Title>{{ title }}</Title>      
+      <Meta name="description" :content="title" />
+      <Meta property="og:title" content="C'est un titre og" />
+      <Style type="text/css" children="body { background-color: green; }" />    
+    </Head>
     <h1>Nouvelle HP</h1>
   </div>
 </template>
