80c5c43be78b7ca14911aeff6e167543593dcc8d
Widdy LOUIS <louisws@outlook.fr>
2023-01-28 21:13:41 +0100

    Head Tags SEO tags part 2

--- nuxt3app/nuxt.config.ts
+++ nuxt3app/nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {    
+    head: {      
+      charset: 'utf-16',      
+      viewport: 'width=500, initial-scale=1',      
+      title: 'My App',      
+      meta: [             
+      { name: 'description', content: 'Ceci est la démo de l\'application Nuxt 3' } ,
+      { property: "og:title",  content:'C\'est un titre og'}     ],
+    }  
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: "https://fakestoreapi.com",
+      appSecret: "",
+    }
+  }
 })
