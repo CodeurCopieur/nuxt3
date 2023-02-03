4ce2f7267ad1ab083725971fe1dc88a7e3a00c9d
Widdy LOUIS <louisws@outlook.fr>
2023-02-03 22:58:48 +0100

    Update default.vue

--- nuxt-movie-app/layouts/default.vue
+++ nuxt-movie-app/layouts/default.vue
@@ -1,22 +1,5 @@
-// <script setup>
-//   const loading = ref(false);
-//   const start = () => {
-//     loading.value = true
-//   };
-//   const finish = () => {
-//     loading.value = false
-//   };
-
-//   onMounted(() => {
-//     start();
-//     setTimeout(() => finish(), 500);
-//   });
-
-  
-// </script>
 <template>
  <div class="min-h-screen">
-  <Loading v-if="loading"/>
   <Header/>
   <main>
     <slot />
