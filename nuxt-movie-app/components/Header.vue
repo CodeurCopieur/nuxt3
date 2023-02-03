1d68867c27fe5345780cccee655a3372c0567d92
Widdy LOUIS <louisws@outlook.fr>
2023-02-03 22:53:46 +0100

    Update Header.vue

--- nuxt-movie-app/components/Header.vue
+++ nuxt-movie-app/components/Header.vue
@@ -1,9 +1,12 @@
-<script setup>
-  
-</script>
-
 <template>
   <header class="shadow-sm bg-white">
-      <h1>Header </h1>
-</header>
+    <nav class="container mx-auto p-4 flex justify-between">
+      <NuxtLink to="/" class="font-bold">CodeurCopieur</NuxtLink>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/movies">Movies</NuxtLink></li>
+        <li><NuxtLink to="/tv">TV Series</NuxtLink></li>
+        <li><NuxtLink to="/search">Search</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
 </template>
