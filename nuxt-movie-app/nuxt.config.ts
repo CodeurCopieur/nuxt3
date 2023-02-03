ef87ed00b0482c84274348686654d31e2b7917e0
Widdy LOUIS <louisws@outlook.fr>
2023-02-03 22:53:25 +0100

    transition page, class body

--- nuxt-movie-app/nuxt.config.ts
+++ nuxt-movie-app/nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {    
+    head: {      
+      charset: 'utf-16',      
+      viewport: 'width=device-width, initial-scale=1',      
+      title: 'Movie Website',      
+      meta: [             
+      { name: 'description', content: 'Movie Website with Nuxt 3 and Themoviedb' } ,
+      { property: "og:title",  content:'Movie Website with Nuxt 3'}],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ],
+      bodyAttrs: {    class: 'min-h-screen'  },
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },  
+  },  
   runtimeConfig: {
     public: {
       baseUrlMovie: "https://api.themoviedb.org/3/movie/",
@@ -7,4 +22,5 @@ export default defineNuxtConfig({
     },
     
   },
+  modules: ['@nuxtjs/tailwindcss'],
 })
