918a1fcbe73bb9782b7a002ec6f307a9113b95c2
Widdy LOUIS <louisws@outlook.fr>
2023-02-05 17:56:48 +0100

    Genre Index Page Design

---
+++ nuxt-movie-app/pages/genres/index.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+<template>
+  <main>
+    <PageHeader title="Genre Index"></PageHeader>
+    <section class="py-10 container mx-auto">
+      <div class="flex flex-wrap gap-5">
+        <NuxtLink 
+          v-for="i in 20"
+          :key="i"
+          to="/genres/test"
+          class="block py-2 px-4 bg-gray-300 rounded hover:bg-gray-400"
+          >#GenreName</NuxtLink>
+      </div>
+    </section>
+  </main>
+</template>
+
+
+
+<style lang="scss" scoped>
+  
+</style>
