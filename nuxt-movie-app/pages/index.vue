7c78a3fc6260406df08274b6b6d40616875c4198
Widdy LOUIS <louisws@outlook.fr>
2023-02-05 02:24:45 +0100

    Update index.vue

--- nuxt-movie-app/pages/index.vue
+++ nuxt-movie-app/pages/index.vue
@@ -2,5 +2,5 @@
   const { data } = await useFetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cc4c125990f5777406886df6fdb3e266');
 </script>
 <template>
-  <Hero :movies="data"/>
+  <Hero :movies="data" />
 </template>
