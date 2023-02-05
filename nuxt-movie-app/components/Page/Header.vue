164a796930312a856cb575a8e4eb9f8020ca478a
Widdy LOUIS <louisws@outlook.fr>
2023-02-05 17:57:26 +0100

    Page Header Component

---
+++ nuxt-movie-app/components/Page/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+  const props = defineProps(["title"]);
+</script>
+
+<template>
+  <section class="hero">
+    <div class="container mx-auto">
+      <h1 v-if="props.title" class="text-4xl font-bold">{{ props.title }}</h1>
+    </div>
+  </section>
+</template>
+
+<style>
+  .hero {
+    @apply py-16 bg-gray-100;
+  } 
+</style>
