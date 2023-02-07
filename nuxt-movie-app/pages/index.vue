84190679b11a416bee2e434d39e6b65dcdd8be13
Widdy LOUIS <louisws@outlook.fr>
2023-02-07 01:09:06 +0100

    Fetching all movie from Api

--- nuxt-movie-app/pages/index.vue
+++ nuxt-movie-app/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup>
-  const { data } = await useFetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cc4c125990f5777406886df6fdb3e266');
+  // const { data } = await useFetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cc4c125990f5777406886df6fdb3e266');
+  const value = await useMoviesApi().getMovies('now_playing', 1);
 </script>
 <template>
-  <Hero :movies="data" />
+  <Hero :movies="val" />
 </template>
