2957a096a64f76ab4a5b5db277573b6ff2aea21b
Widdy LOUIS <louisws@outlook.fr>
2023-02-07 21:06:39 +0100

    Update index.vue

--- nuxt-movie-app/pages/index.vue
+++ nuxt-movie-app/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-  // const { data } = await useFetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cc4c125990f5777406886df6fdb3e266');
+  // const { data } = await useFetch('https://api.themoviedb.org/3/movie/now_playing?api_key=');
   const value = await useMoviesApi().getMovies('now_playing', 1);
 </script>
 <template>
