cb79837360aa982dfac460d7e2ca8af8cfe63064
Widdy LOUIS <louisws@outlook.fr>
2023-02-08 20:55:53 +0100

    Update nuxt.config.ts

--- nuxt-movie-app/nuxt.config.ts
+++ nuxt-movie-app/nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
       ],
       bodyAttrs: {    class: 'min-h-screen'  },
     },
-    pageTransition: { name: 'page', mode: 'out-in' },  
+    // pageTransition: { name: 'page', mode: 'out-in' },  
   },  
   runtimeConfig: {
     public: {
