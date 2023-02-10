fc40f528021974d576f174857b7b7ce065518386
Widdy LOUIS <louisws@outlook.fr>
2023-02-10 20:06:48 +0100

    Update Header.vue

--- nuxt-movie-app/components/Header.vue
+++ nuxt-movie-app/components/Header.vue
@@ -1,12 +1,40 @@
 <template>
-  <header class="shadow-sm bg-white">
-    <nav class="container mx-auto p-4 flex justify-between">
-      <NuxtLink to="/" class="font-bold">CodeurCopieur</NuxtLink>
-      <ul class="flex gap-4">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/movie">Movies</NuxtLink></li>
-        <li><NuxtLink to="/tv">TV Series</NuxtLink></li>
-        <li><NuxtLink to="/search">Search</NuxtLink></li>
+  <header>
+    <nav  class="nav w-20 sm:sticky  md:fixed min-h-screen text-gray-500 flex justify-center items-center">
+      <ul>
+        <li class="mb-10">
+          <NuxtLink class="hover:text-white" to="/">
+          <svg fill="currentColor" viewBox="0 0 24 24" width="24" height="24">
+              <path class="primary" d="M9 22H5a1 1 0 0 1-1-1V11l8-8 8 8v10a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1zm3-9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"></path>
+              <path class="secondary" d="M12.01 4.42l-8.3 8.3a1 1 0 1 1-1.42-1.41l9.02-9.02a1 1 0 0 1 1.41 0l8.99 9.02a1 1 0 0 1-1.42 1.41l-8.28-8.3z"></path>
+            </svg>
+
+          </NuxtLink>
+        </li>
+        <li class="mb-10">
+          <NuxtLink class="hover:text-white" to="/movie">
+            <svg fill="currentColor" width="24" height="24" viewBox="0 0 24 24"><path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
+          </NuxtLink>
+        </li>
+        <li class="mb-10">
+          <NuxtLink class="hover:text-white" to="/tv">
+            <svg fill="currentColor" width="24" height="24"><path d="M21 6h-8.586l2.293-2.293-1.414-1.414L10 5.586 6.707 2.293 5.293 3.707 7.586 6H3a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1zm-1 3v2h-2V9h2zm-2 4h2v2h-2v-2zM4 8h11v10H4V8z"></path></svg>
+          </NuxtLink>
+          </li>
+        <li class="mb-10">
+          <NuxtLink class="hover:text-white" to="/search">
+              <svg fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
+          </NuxtLink>
+        </li>
+        <li class="mb-10">
+          <NuxtLink class="hover:text-white" to="/">
+             <svg fill="currentColor" width="24" height="24" viewBox="0 0 24 24">
+              <path d="M0 0h24v24H0z" fill="none"></path><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+              <path d="M0 0h24v24H0z" fill="none"></path>
+              </svg>
+          </NuxtLink>
+        </li>
+        
       </ul>
     </nav>
   </header>
