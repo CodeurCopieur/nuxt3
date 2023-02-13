d15e5522ceecb03753a221e67899e97ee511be36
Widdy LOUIS <louisws@outlook.fr>
2023-02-14 00:23:14 +0100

    Create Content.vue

---
+++ nuxt-movie-app/components/Content.vue
@@ -0,0 +1,106 @@
+<template>
+   <div class="home my-8">
+    <div class="mb-12">
+      <img src="../assets/slide1.jpg" class="rounded" alt="">
+    </div>
+    <!-- categories -->
+    <div class="categories flex items-center mb-12 -mx-4">
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/disney_logo.svg" alt="disney">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/pixar_logo.svg" alt="pixar">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/marvel_logo.svg" alt="marvel">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/star_wars_logo.svg" alt="star wars">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 p-4 hover:bg-gray-900 shadow-custom">
+          <img src="../assets/national_geographic_logo.svg" alt="national geographic">
+        </a>
+      </div>
+    </div> 
+    <!-- categories -->
+    
+    <div class="recommended px-6 mb-12">
+      <h2 class="text-gray-500 mb-1">Recommended For You</h2>
+      <div class="flex items-center">
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="recommended px-6 mb-12">
+      <h2 class="text-gray-500 mb-1">Recommended For You</h2>
+      <div class="flex items-center">
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="star wars">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
