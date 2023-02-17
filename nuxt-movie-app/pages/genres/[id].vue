88fbb6cffe1e26cd48e0c074bab0f39d246f2067
Widdy LOUIS <louisws@outlook.fr>
2023-02-18 00:39:22 +0100

    news for the requests to have the films by genre

--- nuxt-movie-app/pages/genres/[id].vue
+++ nuxt-movie-app/pages/genres/[id].vue
@@ -1,11 +1,6 @@
+<script setup>
+  const getMoviesByGenres = await useMoviesApi().getMoviesD(`discover/movie`, 1, `action`);
+</script>
 <template>
  <span>genre</span>
 </template>
-
-<script setup>
-  
-</script>
-
-<style lang="scss" scoped>
-  
-</style>
