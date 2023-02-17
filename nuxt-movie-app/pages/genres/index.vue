da1445530e6a3d842fc0070f59447cfebbf0cf8c
Widdy LOUIS <louisws@outlook.fr>
2023-02-18 00:40:11 +0100

    show list of genres

--- nuxt-movie-app/pages/genres/index.vue
+++ nuxt-movie-app/pages/genres/index.vue
@@ -1,15 +1,20 @@
-<script setup></script>
+<script setup>
+  const getGenres = await useMoviesApi().getGenres();
+  const genres = ref();
+  genres.value = getGenres.data;
+
+</script>
 <template>
   <main>
     <PageHeader title="Genre Index"></PageHeader>
     <section class="py-10 container mx-auto">
       <div class="flex flex-wrap gap-5">
         <NuxtLink 
-          v-for="i in 20"
-          :key="i"
-          to="/genres/1234"
+          v-for="genre in genres.value.genres"
+          :key="genre.id"
+          :to="{query: {id: genre.id}, path:`/genres/${genre.name.toLowerCase()}`}"
           class="block py-2 px-4 bg-gray-300 rounded hover:bg-gray-400"
-          >#GenreName{{ i }}</NuxtLink>
+          >#{{ genre.name }}</NuxtLink>
       </div>
     </section>
   </main>
