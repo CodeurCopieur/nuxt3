27de46b9d7f9b3eba0befe5e6610f77f2121965d
Widdy LOUIS <louisws@outlook.fr>
2023-02-18 00:49:19 +0100

    found id in url

--- nuxt-movie-app/pages/genres/[id].vue
+++ nuxt-movie-app/pages/genres/[id].vue
@@ -1,5 +1,6 @@
 <script setup>
   const getMoviesByGenres = await useMoviesApi().getMoviesD(`discover/movie`, 1, `action`);
+  const {id} = useRoute().params;
 </script>
 <template>
  <span>genre</span>
