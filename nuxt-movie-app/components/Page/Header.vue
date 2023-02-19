8e284d10e4bafaf43239b68e55adab7d304a9e25
Widdy LOUIS <louisws@outlook.fr>
2023-02-19 02:20:41 +0100

    Update Header.vue

--- nuxt-movie-app/components/Page/Header.vue
+++ nuxt-movie-app/components/Page/Header.vue
@@ -4,7 +4,7 @@
 
 <template>
   <section class="hero">
-    <div class="container mx-auto">
+    <div class="container mx-auto max-w-7xl">
       <h1 v-if="props.title" class="text-4xl font-bold">{{ props.title }}</h1>
     </div>
   </section>
