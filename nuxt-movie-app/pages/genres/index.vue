64f1afd1dc862a25c0968fc88c0932162d443f40
Widdy LOUIS <louisws@outlook.fr>
2023-02-19 02:19:23 +0100

    inte css (list genres)

--- nuxt-movie-app/pages/genres/index.vue
+++ nuxt-movie-app/pages/genres/index.vue
@@ -5,23 +5,19 @@
 
 </script>
 <template>
-  <main>
+  <div>
     <PageHeader title="Genre Index"></PageHeader>
     <section class="py-10 container mx-auto">
       <div class="flex flex-wrap gap-5">
         <NuxtLink 
+        class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block block py-2 px-4 bg-gray-300 rounded hover:bg-gray-400"
           v-for="genre in genres.value.genres"
           :key="genre.id"
           :to="{query: {name: genre.name.toLowerCase()}, path:`/genres/${genre.id}`}"
-          class="block py-2 px-4 bg-gray-300 rounded hover:bg-gray-400"
-          >#{{ genre.name }}</NuxtLink>
+          >
+          <span class="relative text-white">#{{ genre.name }}</span>
+          </NuxtLink>
       </div>
     </section>
-  </main>
+  </div>
 </template>
-
-
-
-<style lang="scss" scoped>
-  
-</style>
