541a5ef1753d67ee2a125b55bb461e5c826cf320
Widdy LOUIS <louisws@outlook.fr>
2023-02-19 02:20:28 +0100

    Page : display films of the genre

--- nuxt-movie-app/pages/genres/[id].vue
+++ nuxt-movie-app/pages/genres/[id].vue
@@ -1,7 +1,17 @@
 <script setup>
-  const getMoviesByGenres = await useMoviesApi().getMoviesD(`discover/movie`, 1, `action`);
   const {id} = useRoute().params;
+  const query = useRoute().query;
+  const getMoviesByGenres = await useMoviesApi().getMoviesD(`discover/movie`, 1, `${id}`);
+  const allMovies  = ref();
+  allMovies.value = getMoviesByGenres;
+
 </script>
 <template>
- <span>genre</span>
+  <div>
+    <PageHeader :title="query.name.toUpperCase()"></PageHeader>
+    <section class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl  grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+
+      <MovieCard :movies="allMovies"/>
+    </section>
+  </div>
 </template>
