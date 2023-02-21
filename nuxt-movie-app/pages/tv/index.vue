9413d4973905ff1714851cb2ab41364d360ecb8f
Widdy LOUIS <louisws@outlook.fr>
2023-02-21 01:38:07 +0100

    Create index.vue

---
+++ nuxt-movie-app/pages/tv/index.vue
@@ -0,0 +1,7 @@
+<template>
+ <PageHeader title="TV"></PageHeader>
+ <section class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl  grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <MovieOrTvCard :type="`tv`"/>
+    </section>
+</template>
+
