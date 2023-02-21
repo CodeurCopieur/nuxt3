ffe68fcc62ab1c3651b9d47a3e7227015212eab1
Widdy LOUIS <louisws@outlook.fr>
2023-02-21 01:40:50 +0100

    Create MovieOrTvCard.vue

---
+++ nuxt-movie-app/components/MovieOrTvCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const { type } = defineProps(['type']);
+    const {data} = await useMoviesApi().getMoviesD(`discover/${type}`, 1, null);
+</script>
+
+<template>
+  <NuxtLink
+    v-for="(movie, i) in data.results" :key="i" 
+    :to="{ path:`/${type}/${movie.id}`}">
+      <div class=" shadow-lg border border-gray-700 component-app__wrap-movieCard relative overflow-hidden">
+          <picture>
+            <img :src="`https://image.tmdb.org/t/p/original/${movie.poster_path}`" :alt="movie.original_title">
+          </picture>
+          <div class="component-app__wrap-movieCard-info">
+            <span class="mt-8 text-xs text-lg inline-block">{{ useMoviesApi().showYear(`${movie.release_date}`) || useMoviesApi().showYear(`${movie.first_air_date}`) }}</span><br>
+            <div class="precent-bar mt-8">
+              <span class="precent-per inline-block" :style="{'width':useMoviesApi().percent(`${movie.vote_average}`)+'%'}">
+                <span class="percent-tooltip inline-block">{{ useMoviesApi().percent(`${movie.vote_average}`) }} %</span>
+              </span>
+            </div>
+          </div>
+      </div>
+      <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-[#111827] relative inline-block before:border before:border-gray-700 ">
+        <span class="relative text-white">{{ movie.original_title ||movie.original_name }}</span>
+        </span>
+  </NuxtLink>
+</template>
