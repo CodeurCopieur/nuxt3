2772722a2063e0c6fd0d5a536d1e33368fd3fab1
Widdy LOUIS <louisws@outlook.fr>
2023-02-21 01:41:12 +0100

    Update [id].vue

--- nuxt-movie-app/pages/genres/[id].vue
+++ nuxt-movie-app/pages/genres/[id].vue
@@ -1,9 +1,5 @@
 <script setup>
-  const {id} = useRoute().params;
   const query = useRoute().query;
-  const getMoviesByGenres = await useMoviesApi().getMoviesD(`discover/movie`, 1, `${id}`);
-  const allMovies  = ref();
-  allMovies.value = getMoviesByGenres;
 
 </script>
 <template>
@@ -11,7 +7,7 @@
     <PageHeader :title="query.name.toUpperCase()"></PageHeader>
     <section class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl  grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 
-      <MovieCard :movies="allMovies"/>
+      <MovieOrTvCardGenre :type="`movie`"/>
     </section>
   </div>
 </template>
