d0585b54f6a2226e6f7af158837dd7d3896634c3
Widdy LOUIS <louisws@outlook.fr>
2023-02-22 01:30:08 +0100

    Update [id].vue

--- nuxt-movie-app/pages/movie/[id].vue
+++ nuxt-movie-app/pages/movie/[id].vue
@@ -1,10 +1,3 @@
-<script setup>
-  const {id} = useRoute().params;
-  const getDetails = await useMoviesApi().getDetails(`movie/${id}`);
-  const movie  = ref();
-  movie.value = getDetails;
-</script>
-
 <template>
-    <MovieDetails :movie="movie" />
+    <MovieOrTvDetails :type="`movie`" />
 </template>
