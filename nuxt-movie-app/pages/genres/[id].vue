b972dcc759bd920d4b7288da6777b419b70efd3e
Widdy LOUIS <louisws@outlook.fr>
2023-02-26 19:39:34 +0100

    Update [id].vue

--- nuxt-movie-app/pages/genres/[id].vue
+++ nuxt-movie-app/pages/genres/[id].vue
@@ -5,9 +5,6 @@
 <template>
   <div>
     <PageHeader :title="query.name.toUpperCase()"></PageHeader>
-    <section class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl  grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-
-      <MovieOrTvCardGenre :type="`movie`"/>
-    </section>
+    <MovieOrTvCardGenre type="movie"/>
   </div>
 </template>
