f9ad2d04e094156eb0410b75fe3b310a786369af
Widdy LOUIS <louisws@outlook.fr>
2023-02-26 19:40:24 +0100

    Update index.vue

--- nuxt-movie-app/pages/movie/index.vue
+++ nuxt-movie-app/pages/movie/index.vue
@@ -7,7 +7,7 @@
 
 <template>
  <PageHeader title="Movie"></PageHeader>
- <section class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl  grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+ <section class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl  grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
   <MovieOrTvCard :type="`movie`"/>
 </section>
 </template>
