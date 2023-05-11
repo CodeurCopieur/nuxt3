a30928b249c5a45222d1dba0a5b99fefdb89c138
Widdy LOUIS <louisws@outlook.fr>
2023-05-11 13:04:34 +0200

    Update Header.vue

--- nuxt-movie-app/components/Page/Header.vue
+++ nuxt-movie-app/components/Page/Header.vue
@@ -6,9 +6,7 @@
   <section class="hero">
     <div class="mx-4">
       <h1 v-if="props.type" class="text-4xl font-bold text-center">{{ props.title }} - {{ props.type }}</h1>
-      <div v-else>
-        <h1  class="text-4xl font-bold text-center">{{ props.title }}</h1>
-      </div>
+      <h1 v-else class="text-4xl font-bold text-center">{{ props.title }}</h1>
     </div>
   </section>
 </template>
