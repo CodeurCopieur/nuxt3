c642583fe4f401b7c0e50d1dd2dce4814b8cafd8
Widdy LOUIS <louisws@outlook.fr>
2023-05-11 18:14:20 +0200

    implementation of the click system to load the data of the next page

--- nuxt-movie-app/components/MovieOrTvCardGenre.vue
+++ nuxt-movie-app/components/MovieOrTvCardGenre.vue
@@ -1,34 +1,55 @@
 <script setup>
 
-  const { type } = defineProps(['type']);
   const router = useRouter();
   const {id} = useRoute().params;
-  const {name} = useRoute().query;
+  const {name, type} = useRoute().query;
   const page = ref(1);
   const movies = ref([]);
+  const mainOffsetTRef = ref(null)
 
   movies.value = await useMoviesApi().getMoviesD(`discover/${type}`, page.value, `${id}`);
 
-if (name === '' || id === '') {
-  router.push('/genres')
-} else {
-  router.push({query: {name: name, page: page.value}, path:`/genres/${id}`})
-}
+ const mainOffsetT = mainOffsetTRef.value
+
+
+  if (name === '' || type === '' || id === ''  && type === ''   && page && name) {
+    router.push('/genres')
+  } else {
+    router.push({query: {type: type, name: name, page: page.value}, path:`/genres/${id}`})
+  }
     
 
 
-  async function more(e) {
-    page.value++
+  async function pageCall(resBool) {
+    window.scrollTo({
+      top: mainOffsetT,
+      behavior: "smooth"
+    })
+
+    if (resBool) {
+      page.value++
+    } else {
+      page.value--
+    }
   
-    
-    router.push({query: {name: name, page: page.value}, path:`/genres/${id}`})
-      movies.value = await useMoviesApi().getMoviesD(`discover/${type}`, page.value, `${id}`)
+    movies.value = ''
+    router.push({query: {type: type, name: name, page: page.value}, path:`/genres/${id}`})
+    movies.value = await useMoviesApi().getMoviesD(`discover/${type}`, page.value, `${id}`)
   };
 
 </script>
+<template ref="mainOffsetTRef">
 
-<template>
-    <NuxtLink
+<div class="container max-w-xs mx-auto pagination">
+    <div class="flex justify-center items-center">
+      <span class="my-5 w-full" @click="pageCall(false)" v-if="page > 1">P</span>  
+      <span class="my-5 w-full current"> {{ page }}</span>  
+      <span class="my-5 w-full" @click="pageCall(true)" >N</span>
+      
+    </div>
+</div>
+<div class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+   <NuxtLink
       v-for="(movie, i) in movies" :key="i" 
       :to="{ path:`/${type}/${movie.id}`}">
         <div class=" shadow-lg border border-gray-700 component-app__wrap-movieCard relative overflow-hidden">
@@ -51,5 +72,7 @@ if (name === '' || id === '') {
     </NuxtLink>
 
     
-  <button class="my-5 w-full" @click="more" >+</button>
+</div>
+   
+
 </template>
