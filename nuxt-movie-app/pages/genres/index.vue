94b2215826eb8c49f9487ba144e9880a85980826
Widdy LOUIS <louisws@outlook.fr>
2023-05-11 13:02:43 +0200

    added genre list for series

--- nuxt-movie-app/pages/genres/index.vue
+++ nuxt-movie-app/pages/genres/index.vue
@@ -1,19 +1,37 @@
 <script setup>
-  const getGenres = await useMoviesApi().getGenres();
-  const genres = getGenres.data.genres;
+  const getGenres = await useMoviesApi().getGenres(`movie`);
+  const getGenresTv = await useMoviesApi().getGenres(`tv`);
+  const genresMovie = getGenres.data.genres;
+  const genresTv = getGenresTv.data.genres;
+
 
 </script>
 <template>
   <div>
-    <PageHeader title="Genre"></PageHeader>
+    <PageHeader title="Genre" type="Movie"></PageHeader>
 
     <section class="py-10 container max-w-7xl mx-auto">
       <div class="flex flex-wrap gap-5">
         <NuxtLink 
         class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block block py-2 px-4 bg-gray-300 rounded hover:bg-gray-400"
-          v-for="genre in genres"
+          v-for="genre in genresMovie"
           :key="genre.id"
-          :to="{query: {name: genre.name.toLowerCase(), page: 1}, path:`/genres/${genre.id}`}"
+          :to="{query: {type: 'movie', name: genre.name.toLowerCase(), page: 1}, path:`/genres/${genre.id}`}"
+          >
+          <span class="relative text-white">#{{ genre.name }}</span>
+          </NuxtLink>
+      </div>
+    </section>
+
+    <PageHeader title="Genre" type="TV"></PageHeader>
+
+    <section class="py-10 container max-w-7xl mx-auto">
+      <div class="flex flex-wrap gap-5">
+        <NuxtLink 
+        class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block block py-2 px-4 bg-gray-300 rounded hover:bg-gray-400"
+          v-for="genre in genresTv"
+          :key="genre.id"
+          :to="{query: {type: 'tv', name: genre.name.toLowerCase(), page: 1}, path:`/genres/${genre.id}`}"
           >
           <span class="relative text-white">#{{ genre.name }}</span>
           </NuxtLink>
