b84c0f66939b5a585dbc32ddcb840e7bee17a0d9
Widdy LOUIS <louisws@outlook.fr>
2023-05-14 14:31:15 +0200

    Update Content.vue

--- nuxt-movie-app/components/Content.vue
+++ nuxt-movie-app/components/Content.vue
@@ -2,22 +2,22 @@
    <div class="home my-8 container max-w-screen-lg max-w-screen-xl mx-auto px-4 py-8 lg:max-w-7xl">
     
     <div class="recommended px-6 mb-12">
-      <h2 class="text-gray-500 mb-1">Films populaires</h2>
+      <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Films populaires</span></h2>
       <MovieOrTvSlider type="movie" theme="popular"/>
     </div>
 
     <div class="recommended px-6 mb-12">
-      <h2 class="text-gray-500 mb-1">Séries populaires</h2>
+      <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Séries populaires</span></h2>
       <MovieOrTvSlider type="tv" theme="popular"/>
     </div>
 
     <div class="recommended px-6 mb-12">
-      <h2 class="text-gray-500 mb-1">Films les mieux notés</h2>
+      <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Films les mieux notés</span></h2>
       <MovieOrTvSlider type="movie" theme="top_rated"/>
     </div>
 
     <div class="recommended px-6 mb-12">
-      <h2 class="text-gray-500 mb-1">Séries les mieux notés</h2>
+      <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Séries les mieux notés</span></h2>
       <MovieOrTvSlider type="tv" theme="top_rated"/>
     </div>
   </div>
