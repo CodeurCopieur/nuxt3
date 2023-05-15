97ef1002a03a6c5b42ab6341396f1c9c49f25734
Widdy LOUIS <louisws@outlook.fr>
2023-05-15 23:53:09 +0200

    Update MovieOrTvDetails.vue

--- nuxt-movie-app/components/MovieOrTvDetails.vue
+++ nuxt-movie-app/components/MovieOrTvDetails.vue
@@ -1,95 +1,76 @@
 <script setup>
   const { type } = defineProps(['type']);
   const {id} = useRoute().params;
+
   const data = await useMoviesApi().getDetails(`${type}/${id}`);
 
 </script>
 <template>
-   <section class="container max-w-7xl mx-auto py-10 sm:py-16">
+    <section class="container max-w-7xl mx-auto py-10 sm:py-16">
     <!-- Post Title -->
     <div class="text-center mb-10">
       <h1 class="text-3xl sm:text-5xl font-bold">
-        {{ data.title || data.original_name }}
+        {{ data.title || data.original_name || data.name }}
       </h1>
     </div>
     <!-- Post Meta -->
     <div class="text-center mb-8">
-      <span>{{ useMoviesApi().showDate( `${data.release_date || data.first_air_date}`) }}</span>
+      <span v-if="type === 'movie' || type ==='tv'">{{ useMoviesApi().showDate( `${data.release_date || data.first_air_date}`) }}</span>
+      <span v-else>{{ data.known_for_department }}</span>
     </div>
     <!-- Post Image -->
     <div class="postImage w-full rounded mb-10 relative">
       <div class="postImage-bck h-[350px] sm:h-[550px] relative overflow-hidden">
         <div class="postImage__aspect-ratio"></div>
         <picture>
-          <img 
+          <img v-if="type === 'movie' || type ==='tv'"
           :src="`https://image.tmdb.org/t/p/original${data.backdrop_path}`"
           :alt="`${data.original_title || data.original_name}`" />
+          <img v-else
+          :src="`https://image.tmdb.org/t/p/original${data.profile_path}`"
+          :alt="`${data.name}`" />
         </picture>
       </div>
       <div class="postImage-pst">
         <div class="postImage-cover relative">
           <div class="postImage-cover__aspect-ratio"></div>
           <picture>
-            <img :src="`https://image.tmdb.org/t/p/original${data.poster_path}`"
+            <img 
+              v-if="type === 'movie' || type ==='tv'"
+              :src="`https://image.tmdb.org/t/p/original${data.poster_path}`"
               :alt="`${data.original_title || data.original_name}`">
+            <img v-else :src="`https://image.tmdb.org/t/p/original${data.profile_path}`"
+              :alt="`${data.name}`">
           </picture>
         </div>
         <div class="postImage-pst__info h-full">
           <div>
-            <h4 class="text-xs text-2xl sm:text-3xl font-bold mb-12">{{ data.original_title || data.original_name }}</h4>
-            <div class="precent-bar">
-              <span class="precent-per inline-block" :style="{'width':useMoviesApi().percent(`${data.vote_average}`)+'%'}">
-                <span class="percent-tooltip inline-block">{{ useMoviesApi().percent(`${data.vote_average}`) }} %</span>
+            <h4 class="text-xs text-2xl sm:text-3xl font-bold mb-12">{{ data.original_title || data.original_name || data.name }}</h4>
+
+            <div v-if="type === 'movie' && data.popularity > 1 || type ==='tv' && data.popularity > 1" class="precent-bar" >
+              <span class="precent-per inline-block" 
+                :style="{'width':useMoviesApi().percent(`${parseInt(data.vote_average)}`)+'%'}">
+                <span class="percent-tooltip inline-block">{{ useMoviesApi().percent(`${parseInt(data.vote_average)}`) }}</span>
               </span>
             </div>
+
+            <div v-else-if="type === 'person' && data.popularity > 1"  class="precent-bar" >
+              <span class="precent-per inline-block">
+                <span class="percent-tooltip inline-block">{{ parseInt(data.popularity) }}</span>
+              </span>
+            </div>
+
+            <div v-else>
+              <div class="percent-tooltip inline-block text-xs">0</div>
+              <div class="precent-bar">
+                <span class="precent-per inline-block" :style="{'width':110+'%'}"></span>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
     </div>
     <!-- Post Content -->
-  </section>
+    </section>
 </template>
-
-
-<style >
-  .blog__content {
-  @apply sm:px-10;
-}
-.blog__content h1,
-.blog__content h2,
-.blog__content h3,
-.blog__content h4,
-.blog__content h5,
-.blog__content h6,
-.blog__content p {
-  @apply my-5;
-}
-.blog__content h1,
-.blog__content h2,
-.blog__content h3,
-.blog__content h4,
-.blog__content h5,
-.blog__content h6 {
-  @apply font-bold;
-}
-
-.blog__content h1 {
-  @apply text-2xl sm:text-4xl;
-}
-
-.blog__content h2 {
-  @apply text-xl sm:text-3xl;
-}
-
-.blog__content h3 {
-  @apply text-lg sm:text-2xl;
-}
-
-.blog__content h4 {
-  @apply sm:text-xl;
-}
-
-.blog__content h5 {
-  @apply text-sm sm:text-lg;
-}
-</style>
