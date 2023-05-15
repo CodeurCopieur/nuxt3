a00e17a480cf06d8692b2295dea55ec14cfb08db
Widdy LOUIS <louisws@outlook.fr>
2023-05-15 23:53:20 +0200

    Update MovieOrTvCardGenre.vue

--- nuxt-movie-app/components/MovieOrTvCardGenre.vue
+++ nuxt-movie-app/components/MovieOrTvCardGenre.vue
@@ -74,11 +74,16 @@
             </picture>
             <div class="component-app__wrap-movieCard-info">
               <span class="mt-8 text-xs text-lg inline-block">{{ useMoviesApi().showYear(`${movie.release_date}`) || useMoviesApi().showYear(`${movie.first_air_date}`) }}</span><br>
-              <div class="precent-bar mt-8">
-                <span class="precent-per inline-block" :style="{'width':useMoviesApi().percent(`${movie.vote_average}`)+'%'}" v-if="useMoviesApi().percent(`${movie.vote_average}`) > 1">
-                  <span class="percent-tooltip inline-block">{{ useMoviesApi().percent(`${movie.vote_average}`) }} %</span>
-                </span>
-                <span v-else class="precent-per inline-block" style="width: 0">0 %</span>
+              <div class="precent-bar mt-10" v-if="useMoviesApi().percent(`${movie.vote_average}`) > 1">
+              <span class="precent-per inline-block" :style="{'width':useMoviesApi().percent(`${movie.vote_average}`)+'%'}">
+                <span class="percent-tooltip inline-block">{{ useMoviesApi().percent(`${movie.vote_average}`) }}</span>
+              </span>
+              </div>
+              <div v-else>
+                <div class="percent-tooltip inline-block text-xs">0</div>
+                <div class="precent-bar">
+                  <span class="precent-per inline-block" :style="{'width':0}"></span>
+                </div>
               </div>
             </div>
         </div>
