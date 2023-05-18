54cd09a26e0da7be5ca65e66d815f38610917074
Widdy LOUIS <louisws@outlook.fr>
2023-05-18 17:01:01 +0200

    Update MovieOrTvCardGenre.vue

--- nuxt-movie-app/components/MovieOrTvCardGenre.vue
+++ nuxt-movie-app/components/MovieOrTvCardGenre.vue
@@ -63,8 +63,15 @@
       <span class="my-5 w-full" @click="pageCall(true)" >N</span>
       
     </div>
+</div>   
+
+<div 
+  v-if="movies.length"
+  class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+
+  <MovieOrTvOrPersonCard :items="movies" :type="type"></MovieOrTvOrPersonCard>
 </div>
-<div class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+<!-- <div class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
    <NuxtLink
       v-for="(movie, i) in movies" :key="i" 
       :to="{ path:`/${type}/${movie.id}`}">
@@ -93,7 +100,7 @@
     </NuxtLink>
 
     
-</div>
-   
+</div> -->
+
 
 </template>
