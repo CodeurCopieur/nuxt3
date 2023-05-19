5c9615defffb540c157a9ec64300e6f51931d047
Widdy LOUIS <louisws@outlook.fr>
2023-05-19 17:40:49 +0200

    Update index.vue

--- nuxt-movie-app/pages/index.vue
+++ nuxt-movie-app/pages/index.vue
@@ -1,12 +1,13 @@
 <script setup>
   const getMovies = await useMoviesApi().getMovies('movie/now_playing', 1);
+
   const allMovies  = ref();
   allMovies.value = getMovies;
 
 </script>
 <template>
   <div>
-    <Hero :movies="allMovies" />
+    <Hero :movies="allMovies" type="movie" />
     <Content />
   </div>
   
