d7f9b72cb2ba412673b961bd7e3c29437f3d298b
Widdy LOUIS <louisws@outlook.fr>
2023-05-21 21:04:42 +0200

    Update MovieOrTvOrPersonCard.vue

--- nuxt-movie-app/components/MovieOrTvOrPersonCard.vue
+++ nuxt-movie-app/components/MovieOrTvOrPersonCard.vue
@@ -10,7 +10,7 @@
         <div class=" shadow-lg border border-gray-700 component-app__wrap-movieCard relative overflow-hidden">
 
             <picture>
-              <img v-if="type === 'movie' || type ==='tv'" :src="`https://image.tmdb.org/t/p/original/${item.poster_path}`" :alt="item.original_title || item.original_name">
+              <img v-if="type === 'movie' || type === 'tv'" :src="`https://image.tmdb.org/t/p/original/${item.poster_path}`" :alt="item.original_title || item.original_name">
               <img v-else :src="`https://image.tmdb.org/t/p/original/${item.profile_path}`" :alt="item.name || item.original_name">
             </picture>
 
@@ -18,27 +18,30 @@
 
               <span class="mt-8 text-xs text-lg inline-block">{{ useMoviesApi().showYear(`${item.release_date}`) || useMoviesApi().showYear(`${item.first_air_date}`) || item.known_for_department }}</span><br>
 
-              <div class="precent-bar mt-10" v-if="type === 'movie' || type ==='tv'">
+              <div class="precent-bar mt-10" v-if="type === 'movie' && item.popularity > 1 || type === 'tv' && item.popularity > 1 ">
                 <span class="precent-per inline-block" 
-                  :class="useMoviesApi().getColor(`${parseInt(item.vote_average)}`)"
-                  :style="{'width':useMoviesApi().percent(`${item.vote_average}`)+'%'}"     
-                  v-if="useMoviesApi().percent(`${item.vote_average}`) > 1">
+                  :class="useMoviesApi().getColor(`${item.vote_average}`)"
+                  :style="{'width':useMoviesApi().percent(`${item.vote_average}`)+'%'}">
                   <span 
                     class="percent-tooltip inline-block" 
-                    :class="useMoviesApi().getColor(`${parseInt(item.vote_average)}`)">{{ useMoviesApi().percent(`${item.vote_average}`) }}</span>
-                </span>
-                <span v-else class="precent-per inline-block" :style="{'width':0}">
-                  <span class="percent-tooltip inline-block">0</span>
+                    :class="useMoviesApi().getColor(`${item.vote_average}`)">{{ useMoviesApi().percent(`${item.vote_average}`) }}</span>
                 </span>
               </div>
 
-              <div class="precent-bar mt-10" v-if="type === 'person'">
-                <span class="precent-per inline-block" :style="{'width':parseInt(item.popularity)+'%'}"  v-if="item.popularity > 1">
-                  <span class="percent-tooltip inline-block">{{ parseInt(item.popularity) }}</span>
+              <div class="precent-bar mt-10" v-else-if="type === 'person' && item.popularity > 1">
+                <span 
+                  :class="useMoviesApi().getColor(`${parseInt(item.popularity)}`)"
+                  :style="{'width':`${parseInt(item.popularity)}`+'%'}"
+                  class="precent-per inline-block" >
+                  <span class="percent-tooltip inline-block">{{ parseInt(item.popularity) }} lol</span>
                 </span>
-                <span v-else class="precent-per inline-block" :style="{'width':0}">
-                  <span class="percent-tooltip inline-block">0</span>
-                </span>
+              </div>
+
+              <div v-else class="mb-6">
+                <div class="percent-tooltip inline-block text-xs">0</div>
+                  <div class="precent-bar">
+                    <span class="precent-per inline-block" :style="{'width':0+'%'}"></span>
+                  </div>
               </div>
 
             </div>
