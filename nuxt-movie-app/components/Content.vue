596c1d82ebead24e2e2821ca5785b7548da3f244
Widdy LOUIS <louisws@outlook.fr>
2023-05-21 23:09:49 +0200

    using reactive for type props

--- nuxt-movie-app/components/Content.vue
+++ nuxt-movie-app/components/Content.vue
@@ -1,24 +1,31 @@
+<script setup>
+    const state = reactive({
+      movie: 'movie',
+      tv: 'tv'
+  });
+</script>
+
 <template>
    <div class="home my-8 container max-w-screen-lg max-w-screen-xl mx-auto px-4 py-8 lg:max-w-7xl">
     
     <div class="recommended px-6 mb-12">
       <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Films populaires</span></h2>
-      <MovieOrTvSlider type="movie" theme="popular"/>
+      <MovieOrTvSlider :type="state.movie" theme="popular"/>
     </div>
 
     <div class="recommended px-6 mb-12">
       <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Séries populaires</span></h2>
-      <MovieOrTvSlider type="tv" theme="popular"/>
+      <MovieOrTvSlider :type="state.tv" theme="popular"/>
     </div>
 
     <div class="recommended px-6 mb-12">
       <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Films les mieux notés</span></h2>
-      <MovieOrTvSlider type="movie" theme="top_rated"/>
+      <MovieOrTvSlider :type="state.movie" theme="top_rated"/>
     </div>
 
     <div class="recommended px-6 mb-12">
       <h2 class="text-gray-500 mb-1"><span class="border-b-4 border-blue-800 text-lg">Séries les mieux notés</span></h2>
-      <MovieOrTvSlider type="tv" theme="top_rated"/>
+      <MovieOrTvSlider :type="state.tv" theme="top_rated"/>
     </div>
   </div>
 </template>
