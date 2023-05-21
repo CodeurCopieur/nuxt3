bb60f1110a607323a53fdc94ceae966b97ca5556
Widdy LOUIS <louisws@outlook.fr>
2023-05-21 21:07:13 +0200

    Update MovieOrTvCard.vue

--- nuxt-movie-app/components/MovieOrTvCard.vue
+++ nuxt-movie-app/components/MovieOrTvCard.vue
@@ -52,36 +52,6 @@
     v-if="movies.length"
     class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
 
-    <MovieOrTvOrPersonCard :items="movies" :type="type"></MovieOrTvOrPersonCard>
+    <MovieOrTvOrPersonCard :items="movies" :type="type" />
   </div>
-  <!-- <div class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-      <NuxtLink
-      v-for="(movie, i) in movies" :key="i" 
-      :to="{ path:`/${type}/${movie.id}`}">
-        <div class=" shadow-lg border border-gray-700 component-app__wrap-movieCard relative overflow-hidden">
-            <picture>
-              <img :src="`https://image.tmdb.org/t/p/original/${movie.poster_path}`" :alt="movie.original_title || movie.original_name">
-            </picture>
-            <div class="component-app__wrap-movieCard-info">
-              <span class="mt-8 text-xs text-lg inline-block">{{ useMoviesApi().showYear(`${movie.release_date}`) || useMoviesApi().showYear(`${movie.first_air_date}`) }}</span><br>
-              <div class="precent-bar mt-10" v-if="useMoviesApi().percent(`${movie.vote_average}`) > 1">
-                <span class="precent-per inline-block" :style="{'width':useMoviesApi().percent(`${movie.vote_average}`)+'%'}">
-                  <span class="percent-tooltip inline-block">{{ useMoviesApi().percent(`${movie.vote_average}`) }}</span>
-                </span>              
-              </div> 
-              <div v-else>
-                <div class="percent-tooltip inline-block text-xs">0</div>
-                <div class="precent-bar">
-                  <span class="precent-per inline-block" :style="{'width':0}"></span>
-                </div>
-              </div>
-            </div>
-        </div>
-        <span class="before:block before:absolute before:-inset-1 before:-skew-x-2 before:bg-[#111827] relative inline-block before:border before:border-gray-700 ">
-          <span class="relative text-white">{{ movie.original_title || movie.original_name }}</span>
-          </span>
-    </NuxtLink>
-  </div> -->
-
-
 </template>
