b7897e02c70d57989c63edd8a86630a1df847765
Widdy LOUIS <louisws@outlook.fr>
2023-05-22 22:42:39 +0200

    display the names of the actors

--- nuxt-movie-app/components/MovieOrTvSlider.vue
+++ nuxt-movie-app/components/MovieOrTvSlider.vue
@@ -1,13 +1,11 @@
 <script setup>
   const { type, theme, items } = defineProps(['type', 'theme', 'items']);
   const state = reactive({
-    res: [],
-    dynamicValue : ''
+    res: []
   })
 
   if (type === 'movie' || type === 'tv') {
     state.res = await useMoviesApi().getMovies(`${type}/${theme}`, 1);
-    state.dynamicValue = ''
   } else {
     state.res = items
   }
@@ -59,8 +57,10 @@
     :loop="thumbnailSwiperParams.loop"
     :breakpoints="thumbnailSwiperParams.breakpoints"
     :pagination="thumbnailSwiperParams.pagination">
-    <SwiperSlide v-for="(item, i) in state.res" :key="i" class="h-full shadow-xl mr-0 sm:mr-4 border border-gray-700 hover:bg-gray-900 shadow-custom overflow-hidden">
-      <NuxtLink :to="type ? { path:`/${type}/${item.id}`} : { path:`/person/${item.id}`}">
+    <SwiperSlide v-for="(item, i) in state.res" :key="i" 
+    class="h-full shadow-xl mr-0 sm:mr-4 border border-gray-700 hover:bg-gray-900 shadow-custom"
+    :class="{'overflow-hidden': type === 'movie' || type === 'tv' }">
+      <NuxtLink :to="type ? { path:`/${type}/${item.id}`} : { path:`/person/${item.id}`}" class="relative">
         <div class="relative" style="padding-top: 160%;">
           <picture>
             <img 
@@ -69,7 +69,7 @@
               :src="`https://image.tmdb.org/t/p/original/${item.poster_path}`" :alt="`${item.original_title || item.original_name}`">
             <img v-else 
               class="absolute inset-0 object-cover w-full h-full"
-              :src="`https://image.tmdb.org/t/p/original/${item.profile_path}`" :alt="item.name || item.original_name">
+              :src="`https://image.tmdb.org/t/p/original/${item.profile_path}`" :alt="item.name || item.original_name" :title="item.name || item.original_name">
           </picture>
           <div 
             v-if="type === 'movie' && item.vote_average > 1 || type ==='tv' && item.vote_average > 1"
@@ -80,28 +80,10 @@
                   <span class="percent-tooltip inline-block" :class="useMoviesApi().getColor(`${item.vote_average}`)">{{ useMoviesApi().percent(`${item.vote_average}`) }}</span>
                 </span>
           </div>
-
-          <!-- <div 
-            v-else-if="!type && item.popularity > 1"
-            class="precent-bar">
-              <span 
-                :class="useMoviesApi().getColor(`${parseInt(item.popularity)}`)"
-                :style="{'width':`${parseInt(item.popularity)}`+'%'}"
-                class="precent-per">
-                <span 
-                  :class="useMoviesApi().getColor(`${parseInt(item.popularity)}`)"
-                  class="percent-tooltip inline-block">{{ parseInt(item.popularity) }}</span>
-              </span>
-          </div> -->
-
-          <!-- <div v-else class="precent-bar">
-            <div class="percent-tooltip inline-block text-xs">0</div>
-            <div class="precent-bar">
-              <span class="precent-per inline-block" :style="{'width':0+'%'}"></span>
-            </div>
-          </div> -->
-          
         </div>
+        <span v-if="!type" class="text-center relative block">
+          <span class="relative text-white text-xs">{{ item.original_name }}</span>
+        </span>
       </NuxtLink>
     </SwiperSlide>
   </swiper>
