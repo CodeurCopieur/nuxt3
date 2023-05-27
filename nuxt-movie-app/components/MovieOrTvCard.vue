2e51505dcac2fb4fe24b105c0d9be0a2ab168395
Widdy LOUIS <louisws@outlook.fr>
2023-05-27 21:06:04 +0200

    Update MovieOrTvCard.vue

--- nuxt-movie-app/components/MovieOrTvCard.vue
+++ nuxt-movie-app/components/MovieOrTvCard.vue
@@ -50,7 +50,7 @@
   </div>
   <div 
     v-if="movies.length"
-    class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+    class="container max-w-7xl max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-2 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
 
     <MovieOrTvOrPersonCard :items="movies" :type="type" />
   </div>
