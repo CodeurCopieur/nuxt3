eb21ecac56cbd1c97aaea4c1dd5348b864a01c50
Widdy LOUIS <louisws@outlook.fr>
2023-06-02 00:15:44 +0200

    Update ModalContent.vue

--- nuxt-movie-app/components/ModalContent.vue
+++ nuxt-movie-app/components/ModalContent.vue
@@ -1,16 +1,19 @@
 <script setup>
-  const { items } = defineProps(['items']);
+  const { items, type } = defineProps(['items', 'type']);
   defineEmits(['close']);
 </script>
 
 <template>
   <div class="modal-wrapper relative w-screen h-screen  flex justify-center items-center">
-    <div class="container_story relative h-full w-full md:w-2/3 md:h-4/5 bg-white">
+    <div class="container_story relative w-full md:w-2/3 md:h-4/5 bg-white">
             <button 
-        class="block mx-auto md:mr-0 md:ml-auto text-center text-white bg-blue-800 text-teal-900 hover:bg-blue-500 p-3 mb-4"
+        class="block mx-auto md:mr-0 md:ml-auto text-center text-white bg-blue-800 text-teal-900 hover:bg-blue-500 p-3"
       @click.self="$emit('close')"></button>
 
-      <MovieOrTvSlider :type="'videos'" :items="items"/> 
+      <MovieOrTvSlider 
+        :type="'videos'" 
+        :items="items" 
+        :secondType="type"/> 
     </div>
   </div>
 </template>
